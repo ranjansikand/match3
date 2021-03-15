8952288993a9ab98ccbe1b8fa0f347a29690f27f
ranjansikand <50560977+ranjansikand@users.noreply.github.com>
2021-03-14 20:18:51 -0700

    Add files via upload

---
+++ src/Tile.lua
@@ -0,0 +1,63 @@
+--[[
+    GD50
+    Match-3 Remake
+
+    -- Tile Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The individual tiles that make up our game board. Each Tile can have a
+    color and a variety, with the varietes adding extra points to the matches.
+]]
+
+Tile = Class{}
+
+function Tile:init(x, y, color, variety)
+    
+    -- board positions
+    self.gridX = x
+    self.gridY = y
+
+    -- coordinate positions
+    self.x = (self.gridX - 1) * 32
+    self.y = (self.gridY - 1) * 32
+
+    -- tile appearance/points
+    self.color = color
+    self.variety = variety
+
+    if math.random(20) == 7 then
+        self.notShiny = false
+    else 
+        self.notShiny = true
+    end
+end
+
+function Tile:render(x, y)
+    if self.notShiny then
+        -- draw shadow
+        love.graphics.setColor(34, 32, 52, 255)
+        love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+            self.x + x + 2, self.y + y + 2)
+
+        -- draw tile itself
+        love.graphics.setColor(255, 255, 255, 255)
+        love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+            self.x + x, self.y + y)
+    else
+        -- presumably something that glows has no shadow?
+        -- draw backlight
+        love.graphics.setColor(1, 165/255, 0, 0.8)
+        love.graphics.rectangle('fill', self.x + x - 1, self.y + y - 1, 34, 34, 4)
+
+        --draw tile
+        love.graphics.setColor(255, 255, 255, 255)
+        love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
+            self.x + x, self.y + y)
+        
+        -- draw overlay
+        love.graphics.setColor(1, 1, 0, 0.05)
+        love.graphics.rectangle('fill', self.x + x, self.y + y, 32, 32, 4)
+    end
+end
